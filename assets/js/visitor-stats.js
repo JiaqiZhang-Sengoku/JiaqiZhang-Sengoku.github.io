9cc2c3737b0d5d59845ed6eb585a5d752f2a8b16
Sengoku <sengokuzjq@gmail.com>
2026-08-27 02:36:08 +0800

    20260707

--- assets/js/visitor-stats.js
+++ assets/js/visitor-stats.js
@@ -76,12 +76,11 @@
     var renderedHeight = 500 * scale;
     var visibleHeight = Math.min(renderedHeight, 400);
     var offsetX = ((availableWidth - (875 * scale)) / 2) - (33 * scale);
-    var offsetY = Math.min(0, (visibleHeight - renderedHeight) / 2);
 
     region.style.setProperty("--visitor-map-scale", scale.toFixed(4));
     region.style.setProperty("--visitor-map-height", Math.round(visibleHeight) + "px");
     region.style.setProperty("--visitor-map-offset-x", Math.round(offsetX) + "px");
-    region.style.setProperty("--visitor-map-offset-y", Math.round(offsetY) + "px");
+    region.style.setProperty("--visitor-map-offset-y", "0px");
   }
 
   if (chart && typeof MutationObserver === "function") {
